37b89fea465a94a9adfd9a513bbe26e3677ea4b9
“Justin <jarnv@yahoo.com>
2017-02-10 08:40:02 -0500

    Removed Bootstrap Sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
+//= require bootstrap
 //= require toastr
 //= require turbolinks
 //= require_tree 
+
