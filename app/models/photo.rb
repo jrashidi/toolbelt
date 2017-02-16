04eb8ec43dde322edc69d66bb1d36638019ffb87
“Justin <jarnv@yahoo.com>
2017-02-16 13:01:55 -0500

    Type in Photos model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
-]
+
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
 
